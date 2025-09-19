913c9a017f94049d3b8e9636f83c2ab1737f26da
geepee <melaririgodspower04@gmail.com>
2025-09-19 10:19:05 +0100

    chore: initial terraform code commit

---
+++ vpc.tf
@@ -0,0 +1,75 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = merge(var.tags, { Name = "main-vpc" })
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = merge(var.tags, { Name = "private-subnet" })
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "main-igw" })
+}
+
+# Public route table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "public-rt" })
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# NAT Gateway to allow private subnet outbound access
+resource "aws_eip" "nat_eip" {
+
+  tags = merge(var.tags, { Name = "nat-eip" })
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public.id
+
+  tags = merge(var.tags, { Name = "main-nat" })
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
+# Private route table (routes internet via NAT)
+  resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "private-rt" })
+}
+
+resource "aws_route" "private_via_nat" {
+  route_table_id = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+  depends_on = [aws_nat_gateway.nat]
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
